bfcecf82217beab872faeb2aed9ad8cb9f941f29
Dhanapriya226 <dhanapriyad2004@gmail.com>
2026-01-09 19:17:19 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from modules.demand import demand_forecasting
+import demand_forecasting
 from modules.inventory import inventory_analysis
 from modules.logistics import logistics_dashboard
 from modules.ai_insights import ai_insights
